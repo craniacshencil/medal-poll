bc013f0559bd01622a6241c8fc79abe9183289bf
craniacshencil <rajnikamrajnikam@gmail.com>
2024-07-23 22:09:29 +0530

    Add middleware to validate login credentials
    - Add README.md explaining my sins

---
+++ README.md
@@ -0,0 +1,9 @@
+# Medal-poll
+
+medal based polling system for finding a robust middleground
+
+## Bad practises followed
+
+1. Passwords not hashed
+2. Password openly stored in cookies(no jwt)
+3. Try-catch missing out on some checks
---
+++ frontend/middleware.ts
@@ -0,0 +1,28 @@
+import { FormData } from "@/app/page";
+import { NextResponse, type NextRequest } from "next/server";
+
+export default async function validateCredentials(request: NextRequest) {
+  const username = request.cookies.get("username")?.value;
+  const password = request.cookies.get("password")?.value;
+
+  const formData: FormData = { username, password };
+  const loginURL: string = process.env.NEXT_PUBLIC_URL as string;
+
+  try {
+    const response: Response = await fetch(loginURL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
+    });
+    if (response.status == 200) {
+      return NextResponse.next();
+    }
+    return NextResponse.rewrite(new URL("/", request.url));
+  } catch (err) {
+    return NextResponse.rewrite(new URL("/", request.url));
+  }
+}
+
+export const config = {
+  matcher: ["/polling", "/submission"],
+};
